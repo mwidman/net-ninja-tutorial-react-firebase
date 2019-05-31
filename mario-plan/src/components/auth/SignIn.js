c0054acdc4ce4f58404bcf471d9bfc34587b31ab
mwidman <2118673+mwidman@users.noreply.github.com>
2019-05-30 19:13:12 -0700

    Added firebase auth to SignIn component.

--- mario-plan/src/components/auth/SignIn.js
+++ mario-plan/src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions';
 
 export class SignIn extends Component {
 
@@ -15,9 +17,11 @@ export class SignIn extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    this.props.signIn(this.state);
   }
 
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -32,6 +36,9 @@ export class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="red-text center">
+              { authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -39,5 +46,17 @@ export class SignIn extends Component {
   }
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (credentials) => dispatch(signIn(credentials)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
 
--- mario-plan/src/store/reducers/authReducer.js
+++ mario-plan/src/store/reducers/authReducer.js
@@ -1,10 +1,27 @@
 const initState = {
-
+  authError: null,
 }
 
 const authReducer = (state=initState, action) => {
-  return state;
-
+  switch(action.type) {
+    case 'LOGIN_SUCCESS': {
+      console.log('login success');
+      return {
+        ...state,
+        authError: null,
+      };
+    }
+    case 'LOGIN_ERROR': {
+      console.log('login error');
+      return {
+        ...state,
+        authError: 'Login failed',
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
 
 export default authReducer;
