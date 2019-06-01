531f0e3116e739224e7f1effbc5448b7ba3dbe4b
mwidman <2118673+mwidman@users.noreply.github.com>
2019-05-31 18:20:56 -0700

    Change links in NavBar based on signed in status.

--- mario-plan/src/components/layout/SignedInLinks.js
+++ mario-plan/src/components/layout/SignedInLinks.js
@@ -7,7 +7,7 @@ const SignedInLinks = (props) => {
   return (
     <ul className="right">
       <li><NavLink to='/create'>New Project</NavLink></li>
-      <li><a onClick={props.signOut}>Log Out</a></li>
+      <li><NavLink to='/signin' onClick={props.signOut}>Log Out</NavLink></li>
       <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
     </ul>
   );
