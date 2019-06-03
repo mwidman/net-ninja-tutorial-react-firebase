783fd66dfe37c2bf71ea69b8df6ce1d629d3378f
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-03 15:09:02 -0700

    Modified notification component to display notifications from Firestore.

--- mario-plan/src/components/dashboard/Dashboard.js
+++ mario-plan/src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props;
+    const { projects, auth, notifications } = this.props;
 
     if(!auth.uid) return <Redirect to='/signin' />;
 
@@ -19,7 +19,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects}/>
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
@@ -30,7 +30,8 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
   return {
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications,
   };
 }
 
@@ -39,5 +40,6 @@ export default compose(
   connect(mapStateToProps),
   firestoreConnect([
     { collection: 'projects' },
+    { collection: 'notifications', limit: 3 },
   ])
   )(Dashboard);
